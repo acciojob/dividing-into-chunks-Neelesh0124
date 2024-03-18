4246be6d678bbab3e98961a2fde2b51d9cfa4eea
Neelesh0124 <142391511+Neelesh0124@users.noreply.github.com>
2024-03-18 12:23:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	
+	for (let i = 0; i < arr.length; i++) {
+		let maxSum = 0;
+		for (let j = i; j < arr.length; j++) {
+			maxSum = maxSum + arr[j];
+			if (maxSum<=n) {
+				
+			}
+		}
+	}
 };
 
-const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+//const n = prompt("Enter n: ");
+//alert(JSON.stringify(divide(arr, n)));
